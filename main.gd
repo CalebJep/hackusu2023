1c2be4be14dd36e4cd23d0b53babe42b549276ca
Caleb Paskett <a02168686@aggies.usu.edu>
2023-03-25 15:34:16 -0600

    Added victory

--- main.gd
+++ main.gd
@@ -5,6 +5,7 @@ extends Node3D
 @export var health_scene: PackedScene
 @export var rifle_scene: PackedScene
 @export var shotgun_scene: PackedScene
+@export var goal_scene: PackedScene
 
 func _on_main_menu_start_game():
 	var turrets = get_tree().get_nodes_in_group("TurretSpawn")
@@ -36,19 +37,14 @@ func _on_main_menu_start_game():
 		var shotgun = shotgun_scene.instantiate()
 		shotgun.position = spawn.position
 		add_child(shotgun)
-	
-	var health = health_scene.instantiate()
-	health.position = $Spawns/HealthSpawn.position
-	add_child(health)
-	
-	var rifle = rifle_scene.instantiate()
-	rifle.position = $Spawns/RifleSpawn.position
-	add_child(rifle)
-
-	var shotgun = shotgun_scene.instantiate()
-	shotgun.position = $Spawns/ShotgunSpawn.position
-	add_child(shotgun)
-	
+		
+		
+	var goals = get_tree().get_nodes_in_group("VictorySpawn")
+	for spawn in goals:
+		var goal = goal_scene.instantiate()
+		goal.connect("victory", Callable(self, "_on_goal_victory"))
+		goal.position = spawn.position
+		add_child(goal)
 	
 	$UI.show()
 	$Character.initialize($Spawns/PlayerSpawn.position)
@@ -64,5 +60,24 @@ func _on_character_died():
 	$UI.hide()
 	$"Main Menu".show()
 	$StartCamera.current = true
+	get_tree().call_group("enemy", "queue_free")
+	get_tree().call_group("pickup", "queue_free")
+
+
+func _on_goal_victory():
+	$"Victory".show()
+	get_tree().call_group("enemy", "set_physics_process", "false")
+	$Character.set_physics_process(false)
+	$StartCamera.current = true
+	$Character.position = $Spawns/PlayerSpawn.position
+	$Character.hide()
+	$Character/CollisionShape3D.disabled = true
+	
+	
+	await get_tree().create_timer(3).timeout
+	
+	$"Victory".hide()
+	$UI.hide()
+	$"Main Menu".show()
 	get_tree().call_group("enemy", "queue_free")
 	get_tree().call_group("pickup", "queue_free")
